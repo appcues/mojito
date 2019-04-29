b8cb7a96878466eaf90f64c0b64bba46b86cf3bd
pete gamache <pete@gamache.org>
2019-04-29 19:31:58 -0400

    the most basic Mojito.Pool smoke test, this time including test

---
+++ test/mojito/pool_test.exs
@@ -0,0 +1,35 @@
+defmodule Mojito.PoolTest do
+  use ExSpec, async: false
+  doctest Mojito.Pool
+
+  context "Mojito.Pool.Single" do
+    @http_port Application.get_env(:mojito, :test_server_http_port)
+    @https_port Application.get_env(:mojito, :test_server_https_port)
+
+    defp get(path, opts \\ []) do
+      Mojito.Pool.request(%Mojito.Request{
+        method: :get,
+        url: "http://localhost:#{@http_port}#{path}",
+        opts: opts,
+      })
+    end
+
+    defp get_ssl(path, opts \\ []) do
+      Mojito.Pool.request(%Mojito.Request{
+        method: :get,
+        url: "https://localhost:#{@https_port}#{path}",
+        opts: [transport_opts: [verify: :verify_none]] ++ opts,
+      })
+    end
+
+    it "can make HTTP requests" do
+      assert({:ok, response} = get("/"))
+      assert(200 == response.status_code)
+    end
+
+    it "can make HTTPS requests" do
+      assert({:ok, response} = get_ssl("/"))
+      assert(200 == response.status_code)
+    end
+  end
+end
